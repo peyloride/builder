892ff3568d407296b432e0b7a983e1a54bdfc3ac
Okan Binli <okanb3@gmail.com>
2017-06-11 21:33:33 +0300

    make another linux version for arch (pacman based) systems

---
+++ linux-arch.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+## Exit trap
+trap 'ret=$?; test $ret -ne 0 && printf "failed\n\n" >&2; exit $ret' EXIT
+
+set -e
+
+## Fancy echo
+fancy_echo() {
+  printf "\n%b\n" "$1"
+}
+
+## Distro check
+if ! grep -qiE 'Arch Linux' /etc/os-release
+then
+  fancy_echo "Sorry! we don't currently support that distro."
+  exit 1
+fi
+
+
+fancy_echo "Updating pacman database ..."
+  sudo pacman -Syu
+
+## Git
+fancy_echo "Installing Git, version control system ..."
+  sudo pacman -S --noconfirm git
+
+## Check home bin
+if [ ! -d "$HOME/.bin/" ]; then
+  mkdir "$HOME/.bin"
+fi
+
+if [ ! -f "$HOME/.zshrc" ]; then
+  touch $HOME/.zshrc
+fi
+
+if [[ ":$PATH:" != *":$HOME/.bin:"* ]]; then
+  echo 'export PATH="$HOME/.bin:$PATH"' >> ~/.zshrc
+fi
+
+## ZSH and Oh-My-ZSH
+fancy_echo "Installing ZSH ..."
+  sudo pacman -S --noconfirm zsh
+
+fancy_echo "Setting ZSH as default, please enter your password:"
+  chsh -s $(which zsh)
+
+fancy_echo "Installing Oh-My-ZSH ..."
+  git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+  cp ~/.zshrc ~/.zshrc.orig
+  cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+
+
+if [ ! -n "$ZSH" ]; then
+  ZSH=~/.oh-my-zsh
+fi
+
+## Redis
+fancy_echo "Installing Redis, a good key-value database ..."
+  sudo pacman -S --noconfirm redis
+
+## Extra components
+fancy_echo "Installing ImageMagick, to crop and resize images ..."
+  sudo pacman -S --noconfirm imagemagick
+
+fancy_echo "Installing NodeJS, a Javascript runtime ..."
+  sudo pacman -S --noconfirm nodejs
+
+
+fancy_echo "Installing Devel Package for common tools ..."
+  sudo pacman -S --noconfirm base-devel
+
+
+
+## Rbenv
+if [[ ! -d "$HOME/.rbenv" ]]; then
+  fancy_echo "Installing rbenv, to change Ruby versions ..."
+    git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+
+    if ! grep -qs "rbenv init" ~/.zshrc; then
+      echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
+
+      echo 'eval "$(rbenv init - --no-rehash)"' >> ~/.zshrc
+    fi
+
+    export PATH="$HOME/.rbenv/bin:$PATH"
+    eval "$(rbenv init -)"
+fi
+
+if [[ ! -d "$HOME/.rbenv/plugins/rbenv-gem-rehash" ]]; then
+  fancy_echo "Installing rbenv-gem-rehash so the shell automatically picks up binaries after installing gems with binaries..."
+    git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
+fi
+
+if [[ ! -d "$HOME/.rbenv/plugins/ruby-build" ]]; then
+  fancy_echo "Installing ruby-build, to install Rubies ..."
+    git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+fi
+
+## Ruby environment
+RUBY_VERSION="2.4.1"
+
+fancy_echo "Preveting gem system from installing documentation ..."
+  echo 'gem: --no-ri --no-doc' >> ~/.gemrc
+
+fancy_echo "Installing Ruby $RUBY_VERSION ..."
+  rbenv install $RUBY_VERSION
+
+fancy_echo "Setting $RUBY_VERSION as global default Ruby ..."
+  rbenv global $RUBY_VERSION
+  rbenv rehash
+
+fancy_echo "Updating to latest Rubygems version ..."
+  gem update --system
+
+fancy_echo "Installing Rails ..."
+  gem install rails
+
+clear
+
+fancy_echo "Ready and running ZSH ..."
+  zsh
